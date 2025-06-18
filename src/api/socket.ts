7a1692fbc73cb9b1d62c6645ca4ec46e2e2cacc3
Manish Kumar <143871545+Manish9199@users.noreply.github.com>
2025-06-19 01:59:54 +0530

    Update socket.ts

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -1,7 +1,7 @@
 // src/api/socket.ts
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://yt-video-watch-backend-2.onrender.com';
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false, // Don't connect automatically
